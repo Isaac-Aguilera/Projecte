0a91b7125629f3f65071e316a609b0e29f3016ff
Isaac-Aguilera <isaac.aguilera.29@lacetania.cat>
2021-05-11 16:45:21 +0200

    master

--- Projecte/resources/views/layouts/app.blade.php
+++ Projecte/resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Goocrux</title>
+    <<link rel="icon" href="http://example.com/favicon.png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
